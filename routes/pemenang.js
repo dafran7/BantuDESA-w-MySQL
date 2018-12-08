5515f5f34f310ed3a47d3833155a43ff0319e1e1
rielraharj <rielraharj@gmail.com>
2018-12-08 11:19:26 +0700

    Update pemenang.js

--- routes/pemenang.js
+++ routes/pemenang.js
@@ -17,12 +17,12 @@ router.get('/', function(req, res){
 		for (var i = 0; i < sayembaras.length; i++) {
 			sayembaras[i].tanggal_awal = sayembaras[i].tanggal_awal.toDateString();
 			sayembaras[i].tanggal_akhir = sayembaras[i].tanggal_akhir.toDateString();
-			sayembaras[i].gambar = icon[sayembaras[i].topik];
+			//sayembaras[i].gambar = icon[sayembaras[i].topik];
 		}
 
 		console.log(sayembaras);
 		
-		res.render('head-sayembara', {sayembaras:sayembaras});
+		// res.render('head-sayembara', {sayembaras:sayembaras});
 	});
 });
 
@@ -31,26 +31,19 @@ router.get('/:id', function(req, res){
 	lastUrl = req.originalUrl;
 
 	var db = req.con;
-	var query = 'SELECT * FROM peserta WHERE id_sayembara='+varToString(req.params.id);
+	// var query = 'SELECT * FROM peserta WHERE id_sayembara='+varToString(req.params.id);
 	db.query('SELECT * FROM peserta WHERE id_sayembara=?', [req.params.id], function(err,peserta){
 		if(err) throw err;
 		if(!peserta){
-    		res.render('peserta', {message: 'Peserta not found'});//ini buat apa?
+    		// res.render('peserta', {message: 'Peserta not found'});//ini buat apa?
 		}
-
-		//peserta[0].tanggal_buat = article[0].tanggal_buat.toDateString();
-		//article[0].isi_artikel = article[0].isi_artikel.replace(/\\n/g, '<br><br>');
+		console.log(peserta);
+		//peserta[0].tanggal_buat = article[0].tanggal_buat.toDateString();   WHAT IS THISS
+		//article[0].isi_artikel = article[0].isi_artikel.replace(/\\n/g, '<br><br>'); WHAT IS THISSS
 		var data = JSON.stringify(peserta);
-    	res.render('article', {article:article});//ini buat apa? (2)
+    	// res.render('article', {article:article});//ini buat apa? (2)
 	});
 
-	// Article.getArticleById(req.params.id, function(err, article){
-	// 	if(err) throw err;
-	// 	if(!article){
-    // 		res.render('article', {message: 'Article not found'});
-    // 	}
-    // 	res.render('article', {article:article});
-	// });
 });
 
 module.exports = router;
